e53fb9d17a91f91dc08d81c1a56f1280f646fffe
Ezequiel <ezecavallo@gmail.com>
2021-08-06 19:07:53 -0300

    Player controls

--- src/components/AudioPlayerControls.jsx
+++ src/components/AudioPlayerControls.jsx
@@ -2,17 +2,55 @@ import React from 'react';
 import '../assets/styles/components/AudioPlayerControls.css';
 
 class AudioPlayerControls extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      play: false,
+      volume: 30,
+    }
+    this.togglePlay = this.togglePlay.bind(this);
+  }
+
+  togglePlay() {
+    try {
+      this.setState((state) => {
+        return {
+          play: !state.play
+        }
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  playNextTrack() {
+
+  }
+
+  playPreviousTrack() {
+
+  }
+
+  toggleShuffle() {
+
+  }
+
+  toggleRepeat() {
+
+  }
+
   render() {
     return(
       <div className="mediaplayer__controls">
-        <button className="mediaplayer__controls--shuffle"><span></span></button>
-        <button className="mediaplayer__controls--back"><span></span></button>
-        <button className="mediaplayer__controls--play"><span></span></button>
-        <button className="mediaplayer__controls--next"><span></span></button>
-        <button className="mediaplayer__controls--repeat"><span></span></button>
+        <button onClick={this.toggleShuffle} className="mediaplayer__controls--shuffle"><span></span></button>
+        <button onClick={this.playPreviousTrack} className="mediaplayer__controls--back"><span></span></button>
+        <button onClick={this.togglePlay} className="mediaplayer__controls--play" id={this.state.play ? "" : "pause"}><span></span></button>
+        <button onClick={this.playNextTrack} className="mediaplayer__controls--next"><span></span></button>
+        <button onClick={this.toggleRepeat} className="mediaplayer__controls--repeat"><span></span></button>
         <div className="mediaplayer__controls--volume">
           <button></button>
-          <input type="range" className="volume-slider" id="volume-slider" max="100" value="100" />
+          <input type="range" className="volume-slider" id="volume-slider" max="100" value={this.state.volume} />
         </div>
       </div>
     );
