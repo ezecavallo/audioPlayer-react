74bea42970f8d75e5d6d29eecd31ee9cf9311fcf
Ezequiel <ezecavallo@gmail.com>
2021-08-16 13:19:51 -0300

    Spotify API

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -4,24 +4,22 @@ import AudioPlayerControls from './AudioPlayerControls';
 import AudioPlayerBar from './AudioPlayerBar';
 import '../assets/styles/components/AudioPlayer.css';
 
-class AudioPlayer extends React.Component {
-  render () {
-    return (
-      <div className="mediaplayer">
-        <AudioPlayerControls 
-          instance={this.props.instance}
-          player={this.props.player}
-          access_token={this.props.access_token}
-        />
-        <AudioPlayerBar 
-          instance={this.props.instance}
-          player={this.props.player}
-          items={this.props.items}
-          access_token={this.props.access_token}
-        />
-      </div>
-    );
-  };
+const AudioPlayer = (props) => {
+  return (
+    <div className="mediaplayer">
+      <AudioPlayerControls 
+        instance={props.instance}
+        player={props.player}
+        access_token={props.access_token}
+      />
+      <AudioPlayerBar 
+        instance={props.instance}
+        player={props.player}
+        items={props.items}
+        access_token={props.access_token}
+      />
+    </div>
+  );
 };
 
 export default AudioPlayer;
--- src/components/AudioPlayerSongs.jsx
+++ src/components/AudioPlayerSongs.jsx
@@ -2,45 +2,36 @@ import React from 'react';
 import AudioPlayerCover from './AudioPlayerCover';
 import '../assets/styles/components/AudioPlayerSongs.css';
 
-class AudioPlayerSongs extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-  }
-
-  render() {
-    const { currentTrack, nextTrack, previousTrack } = this.props.items
-    return (
-      <div className="mediaplayer__songs--container">
-        <div className="mediaplayer__songs">
-          <AudioPlayerCover 
-              key={1}
-              title={currentTrack?.name} 
-              cover={currentTrack?.album.images[0].url}
-              position="current"
-          />
-          <AudioPlayerCover 
-              key={2}
-              title={nextTrack?.name} 
-              cover={nextTrack?.album.images[0].url}
-              position="next"
-          />
-          <AudioPlayerCover 
-              key={3}
-              title={previousTrack?.name} 
-              cover={previousTrack?.album?.images[0]?.url}
-              position="back"
-          />
-        </div>
-        <div className="mediaplayer__songs--info">
-          <p className="title">{currentTrack?.name} </p>
-          <p className="artist">{currentTrack?.artists.map((artist) => (artist.name))}</p>
-        </div>
+const AudioPlayerSongs = (props) => {
+  const { currentTrack, nextTrack, previousTrack } = props.items
+  return (
+    <div className="mediaplayer__songs--container">
+      <div className="mediaplayer__songs">
+        <AudioPlayerCover 
+            key={1}
+            title={currentTrack?.name} 
+            cover={currentTrack?.album.images[0].url}
+            position="current"
+        />
+        <AudioPlayerCover 
+            key={2}
+            title={nextTrack?.name} 
+            cover={nextTrack?.album.images[0].url}
+            position="next"
+        />
+        <AudioPlayerCover 
+            key={3}
+            title={previousTrack?.name} 
+            cover={previousTrack?.album?.images[0]?.url}
+            position="back"
+        />
       </div>
-    );
-  };
+      <div className="mediaplayer__songs--info">
+        <p className="title">{currentTrack?.name} </p>
+        <p className="artist">{currentTrack?.artists.map((artist) => (artist.name))}</p>
+      </div>
+    </div>
+  );
 };
 
 export default AudioPlayerSongs;
